6a231f35639297767589b9813f1de93201337310
chandu <hemachandubenuboyina@gmail.com>
2025-08-17 18:31:12 +0530

    fewer updates

---
+++ patterns/half_diamond.py
@@ -0,0 +1,20 @@
+# 4
+# *
+# **
+# ***
+# ****
+# ***
+# **
+# *
+
+
+n=int(input())
+for i in range(2*n-1):
+    if i<n:
+        for j in range(i+1):
+            print("*",end="")
+    else:
+        
+        for j in range(2*n-i-1):
+            print("*",end="")
+    print()
