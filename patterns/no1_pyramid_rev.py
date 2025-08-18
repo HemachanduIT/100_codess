7a72286f062547773dac7c640dfe4a2a7efa8ace
chandu <hemachandubenuboyina@gmail.com>
2025-08-18 23:22:11 +0530

    feat:updates

--- patterns/no1_pyramid_rev.py
+++ patterns/no1_pyramid_rev.py
@@ -6,10 +6,22 @@
 # 5 5 5 5 5
 
 
+
+
 n=int(input())
 for i in range(n):
     for j in range(n-i-1):
         print(" ",end=" ")
+    a=1
     for j in range(i+1):
-        print(i+1,end=" ")
+        # a=1
+        print(i+a,end=" ")
+        a-=1
     print()
+    
+# 5
+#         1 
+#       2 1
+#     3 2 1
+#   4 3 2 1
+# 5 4 3 2 1
