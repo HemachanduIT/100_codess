5f502a1d35dea3a12008ad5f8225136a61f12f47
chandu <hemachandubenuboyina@gmail.com>
2025-08-30 20:08:43 +0530

    updated array codes

---
+++ arrays/sum_of_ele.py
@@ -0,0 +1,9 @@
+# 1 2 3 4 5
+# the sum of elements in array is 15
+
+n=list(map(int,input().split()))
+# s=0
+# for i in n:
+#     s+=i
+# print("the sum of elements in array is",s)
+print("the sum of elements in array is",sum(n))
