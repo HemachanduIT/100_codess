baa635e83c9f0d506ef2e63e5830e803556e8946
chandu <hemachandubenuboyina@gmail.com>
2025-09-02 21:34:24 +0530

    array update

--- arrays/sort_half_asc_desc.py
+++ arrays/sort_half_asc_desc.py
@@ -1,11 +1,15 @@
+# 2 3 6 1 5 9
+# the result sort is 1 2 3 9 6 5
+
+
 s=list(map(int,input().split()))
 s.sort()
 a=s
 i=a
-print(i)
+# print(i)
 r=i[:len(s)//2]
 a.sort(reverse=True)
 b=a
-print(b)c
+# print(b)
 c=r+b[:len(s)//2]
-print(c)
+print("the result sort is",*c)
