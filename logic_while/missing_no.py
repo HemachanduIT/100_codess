123e4db913c4acabc6f15447e2eedddeb6604074
chandu <hemachandubenuboyina@gmail.com>
2025-09-09 23:23:37 +0530

    feat-logic while codes\

---
+++ logic_while/missing_no.py
@@ -0,0 +1,13 @@
+# 3
+# 3 0 1
+# the missing no in the serires is 2
+
+
+n=int(input())
+s=list(map(int,input().split()))
+# a=[]
+for i in range(n+1):
+    if i not in s:
+        # a.append(i)
+        print("the missing no in the serires is",i)
+# print(*a)
