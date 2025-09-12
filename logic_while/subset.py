5a3bc119078139eafc7a7cd33400f4a39ef58dea
chandu <hemachandubenuboyina@gmail.com>
2025-09-12 23:53:53 +0530

    updated codes

--- logic_while/subset.py
+++ logic_while/subset.py
@@ -1,11 +1,16 @@
+
+# 1 2
+# 1 2 3 4 
+# True
+
 s1=input()
 s2=input()
 flag=True
 for i in s1:
     if i not in s2:
         print("False")
-        break
         flag=False
+        break
 if flag:
-    print("True")
+    print("s1 is subset of s2")
     
