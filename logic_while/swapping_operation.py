489702e8cc6c25009cab646684f4f4a601f6e6cd
chandu <hemachandubenuboyina@gmail.com>
2025-09-13 18:39:13 +0530

    updated logic_while codes

---
+++ logic_while/swapping_operation.py
@@ -0,0 +1,21 @@
+# input
+# 5
+# 10 20 30 40 50
+# 1 3
+# 30
+
+# output
+# [10, 40, 30, 20, 50] YES [50, 20, 30, 40, 10]
+
+n=int(input())
+s=list(map(int,input().split()))
+loc1,loc2=map(int,input().split())
+key=int(input())
+a=''
+if loc1<len(s) and loc2<len(s):
+    s[loc1],s[loc2]=s[loc2],s[loc1]
+# print(s)
+if key in s:
+    a='YES'
+
+print(s,a,s[::-1])
