586db232150ce442dbb1be495239d655c29c122a
chandu <hemachandubenuboyina@gmail.com>
2025-09-16 23:59:26 +0530

    updated codes

--- logic_while/punctuation_before_senetenc_removal.py
+++ logic_while/punctuation_before_senetenc_removal.py
@@ -1,17 +1,24 @@
 import string
 def sentenceremoval(s):
     output=[]
-    
+    flag=True
     for word in s:
         res=''
+        res1=''
         for char in word:
             if char not in string.punctuation:
-                res+=char
+                if flag:
+                    res+=char
+                res1+=char
             elif char in string.punctuation:
-                res
+                flag=False
+                continue
         output.append(res)
+        # output.append(res1)
+        print(res1)
         
     return output
+
             
 s=input().split()
 print(sentenceremoval(s))
