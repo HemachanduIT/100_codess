5a14fb95d4aa9ee66db767be5e2b2ab81be5bf2b
chandu <hemachandubenuboyina@gmail.com>
2025-09-24 22:45:41 +0530

    feat:updated codes

---
+++ logic_while/second_occ_1st_no.py
@@ -0,0 +1,18 @@
+from collections import Counter
+def secondOccurence(s):
+    # d=Counter(s)
+    # # for i in s:
+    # #     if i in d:
+    # #         d[i]+=1
+    # #     else:
+    # #         d[i]=1
+    # keys=list(d.keys())
+    res=[]
+    for i in range(len(s)):
+        if s[i] in res:
+            return i
+        else:
+            res.append(s[i])
+        
+s=list(map(int,input().split()))
+print(secondOccurence(s))
