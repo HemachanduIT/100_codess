52601b752b6f457e7b50b07e799cc5ba7ca9b91b
chandu <hemachandubenuboyina@gmail.com>
2025-09-25 19:06:06 +0530

    feat:updated codes:

---
+++ logic_while/duplicate_remove_in_word.py
@@ -0,0 +1,15 @@
+# input:
+#     laddu jalebi barfi    
+# output:  
+#     ['ladu', 'jalebi', 'barfi']
+s=list(map(str,input().split()))
+res=[]
+for i in s:
+    var=''
+    for char in i:
+        if char not in var:
+            var+=char
+    res.append(var)
+print(res)
+        c
+        
