25f4b761779a02185840f2bd0a3e109caeba8652
chandu <hemachandubenuboyina@gmail.com>
2025-09-26 15:39:24 +0530

    updated codes

--- logic_while/secret_locker.py
+++ logic_while/secret_locker.py
@@ -4,15 +4,20 @@
 # output:
 #     949
 
-def secretLocker(n,s):
-    res=''
-    for i in range(n):
-        r=s[i]
-        z=str(r)
-        if i<len(z):
-            res+=z[i]              
-    return res
+# def secretLocker(n,s):
+#     res=''
+#     for i in range(n):
+#         r=s[i]
+#         z=str(r)
+#         if i<len(z):
+#             res+=z[i]              
+#     return res
 n=int(input())
-s=list(map(int,input().split()))
-print(secretLocker(n,s))
+s=list(map(str,input().split()))
+# print(secretLocker(n,s))
 # print(s[0][0])
+res=''
+for i in range(n):
+    if i<len(s[i]):
+        res+=s[i][i]
+print(res)
