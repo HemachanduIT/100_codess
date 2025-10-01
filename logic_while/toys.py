4dc554d32ac33fd74e164365af61714f402dfb58
chandu <hemachandubenuboyina@gmail.com>
2025-10-01 23:20:15 +0530

    feat:updated python codes

---
+++ logic_while/toys.py
@@ -0,0 +1,17 @@
+# input:
+#     7 50
+# output:
+#     1 12 5 111 200 1000 10
+
+def maxtoys(n,k,prices):
+    prices.sort()
+    sum_res=0
+    c=0
+    for i in prices:
+        if i<=k and sum_res<=k:
+            sum_res+=i
+            c+=1
+    return c
+n,k=map(int,input().split())
+prices=list(map(int,input().split()))
+print(maxtoys(n,k,prices))
