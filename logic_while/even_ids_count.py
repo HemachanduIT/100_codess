8f06491d5c0c4ec43b73a489dba7529701ad15ec
chandu <hemachandubenuboyina@gmail.com>
2025-10-03 23:12:37 +0530

    updated logic while codes

--- logic_while/even_ids_count.py
+++ logic_while/even_ids_count.py
@@ -1,10 +1,26 @@
+# input1:
+#     5
+#     432 12 45 100 3
+# output1:
+#     2
+
+# input2:
+#     00 000
+# output2:
+#     1
+
 def even_count(n,s):
     res=0
-    for i in range(n):
-        if len(str(s[i]))%2==0:
+    # for i in range(n):
+    #     if len(str(s[i]))%2==0:
+    #         res+=1
+    
+    for i in s:
+        if len(i)%2==0:
             res+=1
         
     return res
 n=int(input())
-s=list(map(int,input().split()))
+# s=list(map(int,input().split()))
+s=input().split()
 print(even_count(n,s))
