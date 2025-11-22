e8f40ce13349246ac3f5e28c02ef309c24ad1f38
chandu <hemachandubenuboyina@gmail.com>
2025-11-22 09:43:31 +0530

    updated code

--- python_codes/abundant_no.py
+++ python_codes/abundant_no.py
@@ -3,4 +3,4 @@ s=0
 for i in range(1,n):
     if n%i==0:
         s+=i
-print("abundant no") if s>n else print("not an abundant")    
+print("abundant no") if s>n else print("not an abundant number")    
