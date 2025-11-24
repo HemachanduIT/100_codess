8ca2af59a1fc427a4db1a0b490d4385f5dd990f7
chandu <hemachandubenuboyina@gmail.com>
2025-11-24 21:59:21 +0530

    updated codes

--- numbers/gcd.py
+++ numbers/gcd.py
@@ -3,10 +3,10 @@
 # the gcd of 36 and  60 is 12
 
 a,b=int(input()),int(input())
-n1,n2=a,b
+res1,res2=a,b
 while a!=b:
     if a>b:
         a-=b
     else:
         b-=a
-print("the gcd of",n1,"and ",n2,"is",a)
+print("the gcd of",res1,"and ",res2,"is",a)
