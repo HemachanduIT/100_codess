73945003401a8194b30641ae65f9df20bfae8e06
chandu <hemachandubenuboyina@gmail.com>
2025-12-09 23:25:00 +0530

    added java codes

---
+++ java_codes/even_or_odd.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class even_or_odd {
+public static void main(String[]args){
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+if(n%2==0){
+    System.out.println(n+"is even");
+    
+}else{
+    System.out.println(n+"is odd");
+}
+sc.close();
+}
+}
