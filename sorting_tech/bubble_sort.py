17670179b107d88c5a38c2d93d00ce32a34076e2
chandu <hemachandubenuboyina@gmail.com>
2025-12-30 19:24:35 +0530

    updated codes

--- sorting_tech/bubble_sort.py
+++ sorting_tech/bubble_sort.py
@@ -1,12 +1,12 @@
 def bubble_sort(arr):
     n=len(arr)
-    flag=True
-    while flag:
-        flag=False
+    f=True
+    while f:
+        f=False
         for i in range(1,n):
             if arr[i-1]>arr[i]:
                 arr[i-1],arr[i]=arr[i],arr[i-1]
-                flag=True
+                f=True
     return arr
     
 a=[5,4,7,2,1,8,9,-2,-6,-5]
