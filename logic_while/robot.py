02dea8add36e5a99f71bb2505ef6ff1ddba6796a
chandu <hemachandubenuboyina@gmail.com>
2026-01-02 22:12:20 +0530

    updated codes

--- logic_while/robot.py
+++ logic_while/robot.py
@@ -4,13 +4,14 @@
 #     GoStopTurn
 
 s=input()
-res=''
-for i in range(len(s)):
-    if s[i]=='G':
-        res+='Go'
-    elif s[i]=='(' and s[i+1]==')':
-        res+='Stop'
-    elif s[i]=='[' and s[i+1]=='A' and s[i+2]=='L' and s[i+3]==']':
-        res+="Turn"
-print(res)
-        
+# res=''
+def robot(s,res=''):
+    for i in range(len(s)):
+        if s[i]=='G':
+            res+='Go'
+        elif s[i]=='(' and s[i+1]==')':
+            res+='Stop'
+        elif s[i]=='[' and s[i+1]=='A' and s[i+2]=='L' and s[i+3]==']':
+            res+="Turn"
+    print(res)
+robot(s)
